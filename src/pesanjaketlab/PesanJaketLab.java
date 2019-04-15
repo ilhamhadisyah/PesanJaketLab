b4c2f96810c12c053ff837c982e795492e392256
ilhamhadisyah <47851861+ilhamhadisyah@users.noreply.github.com>
2019-04-15 15:52:36 +0700

    Desain butuh koreksi & masukan

--- src/pesanjaketlab/PesanJaketLab.java
+++ src/pesanjaketlab/PesanJaketLab.java
@@ -15,6 +15,8 @@ public class PesanJaketLab {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        ParentPage Parent = new ParentPage();
+        Parent.setVisible(true);
         // TODO code application logic here
     }
     
